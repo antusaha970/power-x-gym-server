3e2ea3e3f463108014ab85e84be48cf670e46147
antu <antusaha970@gmail.com>
2023-04-08 22:06:05 +0600

    updated email

--- email.js
+++ email.js
@@ -1,4 +1,4 @@
-const emailBody = (name, plan) => {
+const emailBody = (name, plan, payment) => {
   return `
   <html>
     <body>
@@ -6,7 +6,11 @@ const emailBody = (name, plan) => {
   
       <p>I wanted to take a moment to personally thank you for choosing Power-X-Gym as your fitness destination! We are thrilled to have you as a part of our community.</p>
   
-      <p>I wanted to remind you that you have selected <strong>${plan}</strong>  and your first class is scheduled on <strong>Sunday</strong>. Please remember to bring your payment for the first class when you come in.</p>
+      <p>I wanted to remind you that you have selected <strong>${plan}</strong>  and your first class is scheduled on <strong>Sunday</strong>. ${
+    !payment
+      ? "Please remember to bring your payment for the first class when you come in."
+      : "Thank you for pay in Advanced"
+  }</p>
   
       <p>Our gym is known for its high energy and motivational atmosphere, so be prepared to have fun while working out! Our certified trainers are passionate about helping our members achieve their fitness goals and will be there to support and guide you every step of the way.</p>
   
--- index.js
+++ index.js
@@ -31,6 +31,42 @@ app.post("/isAdmin", async (req, res) => {
   }
 });
 
+// For sending mail to the users
+app.post("/sendMail", async (req, res) => {
+  const email = req.body.email;
+  const plan = req.body.plan;
+  const payment = req.body.payment;
+  const message = emailBody(email, plan || "Basic", payment);
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: `${process.env.ADMIN_EMAIL}`,
+        pass: `${process.env.ADMIN_PASS}`,
+      },
+    });
+
+    const mailOptions = {
+      from: `${process.env.ADMIN_EMAIL}`,
+      to: `${email}`,
+      subject: "Thank you for choosing Power-X-Gym!",
+      html: `${message}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+        // do something useful
+        res.send(true);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 // Mongodb connection
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.cfh8khq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -60,41 +96,6 @@ async function run() {
       } catch (error) {
         console.log(error);
         res.send(error.message);
-      }
-    });
-
-    // For sending mail to the users
-    app.post("/sendMail", async (req, res) => {
-      const email = req.body.email;
-      const plan = req.body.plan;
-      const message = emailBody(email, plan || "Basic");
-      try {
-        const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: `${process.env.ADMIN_EMAIL}`,
-            pass: `${process.env.ADMIN_PASS}`,
-          },
-        });
-
-        const mailOptions = {
-          from: `${process.env.ADMIN_EMAIL}`,
-          to: `${email}`,
-          subject: "Thank you for choosing Power-X-Gym!",
-          html: `${message}`,
-        };
-
-        transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log("Email sent: " + info.response);
-            // do something useful
-            res.send(true);
-          }
-        });
-      } catch (error) {
-        console.log(error);
       }
     });
 
